4d9bd772182b448e5712ba07d7e40abc47fa12cf
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-11-22 00:44:28 +0100

    vscode prettier formatting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-
 function App() {
   return (
     <div className="container">
-      <h2 className="font-jos text-center text-2xl font-bold">
-        Hello from home!
-      </h2>
+      <h2 className="font-jos text-center text-2xl font-bold">Hello from home!</h2>
     </div>
   );
 }
