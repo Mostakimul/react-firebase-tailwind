ccd939e53c0a6f792cf527707dac5fe4e5fb143a
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-11-22 00:29:35 +0100

    custom font and container extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jos: ['Josefin Sans', 'sans-serif'],
+        mon: ['Montserrat', ' sans-serif'],
+      },
+      container: {
+        center: true,
+      },
+    },
   },
   variants: {
     extend: {},
